5289b49485a0ffbee6131cac894a0ac6358a4f2e
André Medeiros Silva <andre1998vv@gmail.com>
2024-01-17 21:42:32 -0300

    Aulas 11 e 12

---
+++ Aula11/ex008.js
@@ -0,0 +1,10 @@
+// Com a extensão 'Node Exec', F8 para rodar o código com o node js
+
+var vel = 61
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
