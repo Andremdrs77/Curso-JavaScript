9486e059035bf8567ec1d1d14bd30864f703a4f5
André Medeiros Silva <andre1998vv@gmail.com>
2024-02-01 11:57:10 -0300

    Aula 13

---
+++ Aula13/ambiente.js
@@ -0,0 +1,14 @@
+// Estrutura de repetição com teste lógico no início
+var z = 1
+do {
+    console.log(`Passo ${z}`)
+    z++
+} while (z <= 6)
+
+/* 
+Estrutura de repetição com teste lógico no final
+var x = 1
+while (x <= 6) {
+    console.log('Passo ' + x)
+    x++
+} */
