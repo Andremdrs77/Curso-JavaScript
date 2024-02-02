42ecfcd6a1f247404b667d521ed3880e652f1759
André Medeiros Silva <andre1998vv@gmail.com>
2024-02-02 10:19:49 -0300

    Aula 14

---
+++ Aula14/ambiente.js
@@ -0,0 +1,7 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c++) {
+    console.log(c)
+}
+console.log('FIM')
+
+//Usei o modo de depuração (debug) para ver como a máquina interpreta o 'for'.
